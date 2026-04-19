7d5365ce40601d9bab806329847afa37795d4496
RobMaty <rmatyas98@yahoo.com>
2026-04-19 17:29:28 +0300

    chore ( dev dependencies ) add --target flag

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --target /tmp/packages
+export PYTHONPATH=/tmp/packages:$PYTHONPATH
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
